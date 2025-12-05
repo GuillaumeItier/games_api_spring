700032d97008f080a0e0cb711efcb6585616845a
mithridatem <mathieumithridate@adrar-formation.com>
2025-12-05 11:57:36 +0100

    feat : ConsoleController getConsoleById end-point

--- src/main/java/com/adrar/games/controller/ConsoleController.java
+++ src/main/java/com/adrar/games/controller/ConsoleController.java
@@ -5,9 +5,11 @@ import com.adrar.games.repository.ConsoleRepository;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 @RestController
 public class ConsoleController {
@@ -21,6 +23,7 @@ public class ConsoleController {
         this.consoleRepository = consoleRepository;
     }
 
+    //Méthode qui retourne toutes les consoles
     @GetMapping("/consoles")
     public ResponseEntity<ArrayList<Console>> getAllConsoles()
     {
@@ -28,5 +31,21 @@ public class ConsoleController {
         ArrayList<Console> consoles = (ArrayList<Console>) consoleRepository.findAll();
         //Réponse HTTP
         return new ResponseEntity<>(consoles, HttpStatus.OK);
+    }
+
+    //Méthode qui retourne une console avec son id
+    @GetMapping("/console/{id}")
+    public ResponseEntity<Optional<Console>> getConsoleById(@PathVariable Integer id)
+    {
+        //récupération de la console
+        Optional<Console> console = consoleRepository.findById(id);
+
+        //Test si la console existe
+        if (console.isPresent()) {
+            //Retourner réponse HTTP
+            return new ResponseEntity<>(console, HttpStatus.OK);
+        }
+        //Si la console n'existe pas
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
